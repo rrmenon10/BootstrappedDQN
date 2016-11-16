090145165ce7182a137f2f7aee981bdc1b847293
Rakesh R Menon <rockmenon@icloud.com>
2016-11-16 23:07:15 +0530

    Plot file changes

--- DeepMind-Atari-Deep-Q-Learner/dqn/plot_results.lua
+++ DeepMind-Atari-Deep-Q-Learner/dqn/plot_results.lua
@@ -14,27 +14,27 @@ data = torch.load(arg[1])
 
 --gnuplot.raw('set multiplot layout 2, 3')
 
-gnuplot.figure()
+gnuplot.pdffigure('Average_reward.pdf')
 gnuplot.title('Average reward per game during testing')
 gnuplot.plot(torch.Tensor(data.reward_history))
 
-gnuplot.figure()
+gnuplot.pdffigure('Total_count_reward.pdf')
 gnuplot.title('Total count of rewards during testing')
 gnuplot.plot(torch.Tensor(data.reward_counts))
 
-gnuplot.figure()
+gnuplot.pdffigure('Num_games.pdf')
 gnuplot.title('Number of games played during testing')
 gnuplot.plot(torch.Tensor(data.episode_counts))
 
-gnuplot.figure()
+gnuplot.pdffigure('Average_q_val.pdf')
 gnuplot.title('Average Q-value of validation set')
 gnuplot.plot(torch.Tensor(data.v_history))
 
-gnuplot.figure()
+gnuplot.pdffigure('TD error.pdf')
 gnuplot.title('TD error (old and new Q-value difference) of validation set')
 gnuplot.plot(torch.Tensor(data.td_history))
 
-gnuplot.figure()
+gnuplot.pdffigure('Seconds.pdf')
 gnuplot.title('Seconds elapsed after epoch')
 gnuplot.plot(torch.Tensor(data.time_history))
 
